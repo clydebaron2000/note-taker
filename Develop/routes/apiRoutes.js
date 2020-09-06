582572bffa9eb6561ad16740bb10468556220a37
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-09-06 15:25:47 -0700

    fixed id bug and created random length id's for less id collision cases

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,7 +3,7 @@ const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 let path = require("path");
-const idLength = 10;
+const idLength = (10 + ~~(Math.random() * 20));
 module.exports = function(app) {
     app.get("/api/notes", (req, res) => {
         const dbPath = path.join(__dirname, "../db/db.json");
@@ -16,10 +16,9 @@ module.exports = function(app) {
         // req.body is available since we're using the body parsing middleware
         const dbPath = path.join(__dirname, "../db/db.json");
         (async() => {
-            let db = JSON.parse(await readFileAsync(dbPath, 'utf8'));
             let existingIds = [],
-                newId = '',
-                newNote = req.body;
+                newID, newNote = req.body;
+            let db = JSON.parse(await readFileAsync(dbPath, 'utf8'));
             //get existing id's
             db.forEach(data => existingIds.push(data.id));
             //generate new ids until you get a unqiue one
@@ -27,7 +26,7 @@ module.exports = function(app) {
                 newID = [...Array(idLength)].map(i => (Math.floor(Math.random() * 36)).toString(36)).join('');
             } while (existingIds.includes(newID));
             //assign
-            newNote.id = newId;
+            newNote.id = newID;
             //add do js db
             db.push(newNote);
             //save to file
@@ -40,7 +39,7 @@ module.exports = function(app) {
         (async() => {
             let db = JSON.parse(await readFileAsync(dbPath, 'utf8'));
             //get delete index from .find method. req.params returns a string
-            const deleteIndex = db.indexOf(db.find(element => element.id === parseInt(req.params.id))); // get index
+            const deleteIndex = db.indexOf(db.find(element => element.id === req.params.id)); // get index
             db.splice(deleteIndex, 1); //deleting from array
             writeFileAsync(dbPath, JSON.stringify(db)); //save
             res.json(req.body);
